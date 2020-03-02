b52db970a86bbf374f03082a01a6498a685d8676
Ioodu <chinadbo@foxmail.com>
2020-03-02 17:45:43 +0800

    gh-pages

---
+++ build/precache-manifest.91ff7921c1f426622983aefda61ca466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9fef5f3fe46aaf8cf9c8ae4a94d2d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81085d31fc6f22e9d8a7",
+    "url": "/static/css/main.1515c12b.chunk.css"
+  },
+  {
+    "revision": "76d27770b5bd7410d64c",
+    "url": "/static/js/2.79256a6c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.79256a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81085d31fc6f22e9d8a7",
+    "url": "/static/js/main.b79ae27e.chunk.js"
+  },
+  {
+    "revision": "2807fd1ac2ae16ffa310",
+    "url": "/static/js/runtime-main.97709d72.js"
+  }
+]);
